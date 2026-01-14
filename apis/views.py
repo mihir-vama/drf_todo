4ba1c1bf816436ae1cb093c87e636df7b6c2d538
mihirg42 <mihir@vama.app>
2026-01-14 10:11:54 +0530

    hello

--- apis/views.py
+++ apis/views.py
@@ -1,18 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from todos import models
 from .serializers import TodoSerializer
 
 
-class ListTodo(generics.ListCreateAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
     serializer_class = TodoSerializer
-
-    def get_queryset(self):
-        return models.Todo.objects.all()
-
-
-class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = TodoSerializer
-
-    def get_queryset(self):
-        return models.Todo.objects.all()
--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.TextField()
+    password = models.TextField()
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+        null=True, related_name='todo')
 
     def __str__(self):
         return self.title
