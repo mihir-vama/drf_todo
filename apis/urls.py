1f29f1c7f8a985cea6235f7814bb176f980f2c97
mihirg42 <mihir@vama.app>
2026-01-14 10:12:39 +0530

    todo app without user

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListTodo, DetailTodo
 
-urlpatterns = [
-    path('', ListTodo.as_view(), name='todo-list'),
-    path('<int:pk>/', DetailTodo.as_view(), name='todo-detail'),
-]
+from .views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("", TodoViewSet, basename="todos")
+urlpatterns = router.urls
